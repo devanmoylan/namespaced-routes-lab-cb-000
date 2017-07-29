f8e43b9f3426162d2471aa0563a5436607f1c9a6
Devan <devan.moylan@backyardlion.com>
2017-07-29 15:19:30 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,8 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    binding.pry
-    @artist = Artist.new
+    preference = Preference.first
+    if preference && preference.allow_create_artists
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
